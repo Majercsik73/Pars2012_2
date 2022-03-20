86e994eb34a9f850f195a64f2575e68d905e3140
Majercsik73 <riasztotnekem@gmail.com>
2022-03-20 17:33:00 +0100

    Add project files.

---
+++ Pars2012_2/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Pars2012_2
+{
+    class Program
+    {
+        static List<Versenyzo> selejtezo = new List<Versenyzo>();
+        static Dictionary<string, double> eredmenyek = new Dictionary<string, double>();
+
+        static void Main(string[] args)
+        {
+            string[] teljesSelejtezo = File.ReadAllLines("../../../../Selejtezo2012.txt");
+
+            for (int i = 1; i < teljesSelejtezo.Length; i++)
+            {
+                selejtezo.Add(new Versenyzo(teljesSelejtezo[i]));
+            }
+
+            Otodikfeladat();
+
+            Hatodikfeladat();
+
+            Kilencedikfeladat();
+
+            Tizedikfeladat();
+        }
+
+        public static void Tizedikfeladat()
+        {
+            StreamWriter sw = new StreamWriter("Dontos2012.txt");
+            sw.WriteLine("Helyezés;Név;Csoport;Nemzet;NemzetKód;Sorozat;Eredmény");
+
+            for (int i = 1; i < 13; i++)
+            {
+                double legnagyobbdobas = 0.0;
+                string doboneve = "";
+                for (int j = 0; j < eredmenyek.Count; j++)
+                {
+
+                    if (legnagyobbdobas < eredmenyek.ElementAt(j).Value)
+                    {
+                        legnagyobbdobas = eredmenyek.ElementAt(j).Value;
+                        doboneve = eredmenyek.ElementAt(j).Key;
+                    }
+                }
+                //Console.WriteLine($"A legjobb dobó: {doboneve} , eredménye: {legnagyobbdobas}   !");
+
+                for (int k = 0; k < selejtezo.Count; k++)
+                {
+                    if (selejtezo[k].nev == doboneve)
+                    {
+                        sw.WriteLine($"{i};{doboneve};{selejtezo[k].csoport};" +
+                        $"{selejtezo[k].Nemzet};{selejtezo[k].Kod};" +
+                        $"{selejtezo[k].sorozat};{legnagyobbdobas}");
+                    }
+                }
+                eredmenyek.Remove(doboneve);
+            }
+            sw.Close();
+        }
+
+        public static void Kilencedikfeladat()
+        {
+
+            for (int i = 0; i < selejtezo.Count; i++)
+            {
+                eredmenyek.Add(selejtezo[i].nev, selejtezo[i].Eredmeny);
+                Console.WriteLine($"{selejtezo[i].nev} :   {selejtezo[i].Eredmeny}");
+            }
+
+            double legnagyobbdobas = 0.0;
+            string doboneve = "";
+            for (int j = 0; j < eredmenyek.Count; j++)
+            {
+
+                if (legnagyobbdobas < eredmenyek.ElementAt(j).Value)
+                {
+                    legnagyobbdobas = eredmenyek.ElementAt(j).Value;
+                    doboneve = eredmenyek.ElementAt(j).Key;
+                }
+            }
+            //Console.WriteLine($"A legjobb dobó: {doboneve} , eredménye: {legnagyobbdobas}   !");
+
+
+
+            for (int k = 0; k < selejtezo.Count; k++)
+            {
+                if (selejtezo[k].nev == doboneve)
+                {
+                    Console.WriteLine($"9. feladat: A selejtező nyertese:\n\t" +
+                        $"Név: {selejtezo[k].nev}\n\tCsoport: {selejtezo[k].csoport}" +
+                        $"\n\tNemzet: {selejtezo[k].Nemzet}\n\tNemzet kód: {selejtezo[k].Kod}\n\t" +
+                        $"Sorozat: {selejtezo[k].sorozat};\n\tEredmény: {legnagyobbdobas}");
+                }
+            }
+
+        }
+
+        private static void Hatodikfeladat()
+        {
+            int db = 0;
+            for (int i = 0; i < selejtezo.Count; i++)
+            {
+                if (Convert.ToDouble(selejtezo[i].d1) > 78.00 || Convert.ToDouble(selejtezo[i].d2) > 78.00 || Convert.ToDouble(selejtezo[i].d3) > 78.00)
+                {
+                    db++;
+                }
+            }
+
+            Console.WriteLine($"6. feladat: 78,00 méter feletti eredménnyel továbbjutott: {db} fő");
+        }
+
+        private static void Otodikfeladat()
+        {
+            Console.WriteLine($"5. feladat: Versenyzők száma a selejtezőben: {selejtezo.Count} fő!");
+        }
+    }
+}
---
+++ Pars2012_2/Versenyzo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pars2012_2
+{
+    class Versenyzo
+    {
+        public string nev { get; set; }
+        public string csoport { get; set; }
+        public string nemzetEsKod { get; set; }
+        public string sorozat { get; set; }
+        public string d1 { get; set; }
+        public string d2 { get; set; }
+        public string d3 { get; set; }
+
+        public Versenyzo(string sor)
+        {
+            string[] sordarab = sor.Split(";");
+
+            nev = sordarab[0];
+            csoport = sordarab[1];
+            nemzetEsKod = sordarab[2];
+            sorozat = $"{sordarab[3]};{sordarab[4]};{sordarab[5]}";
+            d1 = Konvertal(sordarab[3]);
+            d2 = Konvertal(sordarab[4]);
+            d3 = Konvertal(sordarab[5]);
+        }
+
+        public static string Konvertal(string elem)
+        {
+            if (elem == "X")
+            {
+                return "-1,0";
+            }
+            else if (elem == "-")
+            {
+                return "-2,0";
+            }
+            else return elem;
+        }
+
+        public double Eredmeny
+        {
+            get
+            {
+                double legjobb = 0.0;
+                if (Convert.ToDouble(d1) > Convert.ToDouble(d2))
+                {
+                    if (Convert.ToDouble(d1) > Convert.ToDouble(d3))
+                    {
+                        legjobb = Convert.ToDouble(d1);
+                    }
+                    else
+                    {
+                        legjobb = Convert.ToDouble(d3);
+                    }
+                }
+                else
+                {
+                    if (Convert.ToDouble(d2) > Convert.ToDouble(d3))
+                    {
+                        legjobb = Convert.ToDouble(d2);
+                    }
+                    else
+                    {
+                        legjobb = Convert.ToDouble(d3);
+                    }
+                }
+                return legjobb;
+            }
+        }
+
+        public string Nemzet
+        {
+            get
+            {
+                string[] nemzete = nemzetEsKod.Split("(");
+                string nemzet = nemzete[0].TrimEnd();
+                return nemzet;
+            }
+        }
+
+        public string Kod
+        {
+            get
+            {
+                string[] kodja = nemzetEsKod.Split("(");
+                string kod = kodja[1].Replace(")", "");
+                return kod;
+            }
+        }
+    }
+}
